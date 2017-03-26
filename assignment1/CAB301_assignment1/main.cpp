c3fa44113ee2caf285e105e9cd524eb0d715a4bb
AwolDes <awoldes@gmail.com>
2017-03-26 16:55:02 +1000

    Updates

--- assignment1/CAB301_assignment1/main.cpp
+++ assignment1/CAB301_assignment1/main.cpp
@@ -11,20 +11,16 @@ int BruteForceMedian(int numbers[]) {
 
 
     for( int i = 0; i < (n-1); i++) {
-        //cout << numbers[i] << endl;
 
         int numsSmaller = 0;
         int numsEqual = 0;
-
+        // basic operations - called n-1^2 times
         for (int j = 0; j < (n-1); j++) {
 
             if (numbers[j] < numbers[i]) {
                 numsSmaller += 1;
-
             } else if (numbers[j] == numbers[i]){
-
                 numsEqual += 1;
-
             }
         }
 
@@ -42,18 +38,12 @@ int main()
 
     int nums[n];
 
-    // Generate data
+    // Generate data with the possibility of duplicates
+    // Also randomizes the order
     for (int i = 0; i < n; i = i + 1){
-        nums[i] = i;
+        nums[i] = rand() % (n-1);
     }
 
-    // Shuffle data
-    for (int x = 0; x < (n-1); x++) {
-            int randPos = rand() % (n-1);
-            int tempPos = nums[x];
-            nums[x] = nums[randPos];
-            nums[randPos] = tempPos;
-    }
 
     cout << "Median: " << BruteForceMedian(nums);
 
